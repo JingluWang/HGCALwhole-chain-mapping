ca2222a8b32571705a95f13faed453b14def8f7b
Jinglu Wang <107279970+JingluWang@users.noreply.github.com>
2023-11-13 10:23:12 -0600

    LD

--- eventdataTree.C
+++ eventdataTree.C
@@ -69,12 +69,12 @@ void eventdataTree(){
         eventcounter = i;    
 
         for (unsigned int erx = 0; erx < Nerx; erx++)  {
-            uint16_t cm0 = (erx + 1) * 39 - 2;
-            uint16_t cm1 = (erx + 1) * 39 - 1;
-            channelData[1] = (cm1 & 0x3ff) | ((cm0 & 0x3ff) << 10) | ((cm1 & 0x3ff) << 20);
+            uint32_t cm0 = (erx + 1) * 39 - 2;
+            uint32_t cm1 = (erx + 1) * 39 - 1;
+            channelData[1] = (cm1 & 0x3ff) | ((cm0 & 0x3ff) << 10);
 
-            for (unsigned int ich = 0; ich < 39; ich++) { 
-                unsigned int val = (erx*39 + ich) & 0x3ff;
+            for (unsigned int ich = 2; ich < 39; ich++) { 
+                uint32_t val = (erx*39 + ich) & 0x3ff;
                 channelData[ich+2] = val | (val << 10) | (val << 20); 
             }
 
